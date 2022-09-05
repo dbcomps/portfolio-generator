2c8b5c818de72168e427d1c69f3fb6c72feb4306
David Barraza <dbcomps@mac.com>
2022-09-05 08:13:27 -0500

    created app.js with function and array to take input from command line

---
+++ app.js
@@ -0,0 +1,13 @@
+console.time('coderun')
+
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = profileDataArr => {
+	profileDataArr.forEach((profileItem) => {
+		console.log(profileItem)
+	});
+};
+
+printProfileData(profileDataArgs);
+
+console.timeEnd('coderun')
